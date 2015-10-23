738955a3a8f63463338f1756d6b125828edddad3
jefBinomed <jean.francois.garreau@gmail.com>
2015-10-24 01:02:23 +0200

    feat() mise en place début animation reste a gérer les tirs et les interractions avec les tirs

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
+'use strict';
+
 var DevFestCountDown = DevFestCountDown || function(){
 	
 	// Const
-	var SIZE_LOGO_ORI = 240
+	var SIZE_LOGO_ORI = 240,
 		SIZE_LOGO_DEST = 100, 
-		MARGIN = 20;
+		TIME_ANIMATION = 1000,
+		TIME_ANIMATION_SPACESHIP = 500,
+		TIME_SHOOT = 500,
+		MARGIN = 20,
+		NB_ROW = -1;
 
 	var canvas = null,
 		context = null,
@@ -11,6 +17,12 @@ var DevFestCountDown = DevFestCountDown || function(){
 		audioElt = null,
 		currentTime = new Date().getTime(),
         indexPlaylist = 0,
+        directionLogos = -1,
+        directionSpaceShip = -1,
+        positionShoot =  {
+        	x : -1,
+        	y : -1
+        },
         playListSongs = [
         ],
         positionLogos = {
@@ -41,38 +53,38 @@ var DevFestCountDown = DevFestCountDown || function(){
         },
         positionSpaceShip = {
         	x : 0,
-        	y : 4
+        	y : NB_ROW
         },
         mapLogos = [
         	[
-        		{id:'photos',pos:{x:0,y:0}},
-        		{id:'gplus',pos:{x:1,y:0}},
-        		{id:'music',pos:{x:2,y:0}},
-        		{id:'maps',pos:{x:3,y:0}},
-        		{id:'calendar',pos:{x:4,y:0}},
-        		{id:'keep',pos:{x:5,y:0}},
-        		{id:'glass',pos:{x:6,y:0}},
-        		{id:'android',pos:{x:7,y:0}},
+        		{id:'photos',pos:{x:2,y:0}},
+        		{id:'gplus',pos:{x:3,y:0}},
+        		{id:'music',pos:{x:4,y:0}},
+        		{id:'maps',pos:{x:5,y:0}},
+        		{id:'calendar',pos:{x:6,y:0}},
+        		{id:'keep',pos:{x:7,y:0}},
+        		{id:'glass',pos:{x:8,y:0}},
+        		{id:'android',pos:{x:9,y:0}},
         	],
         	[
-        		{id:'compute',pos:{x:0,y:1}},
-        		{id:'play',pos:{x:1,y:1}},
-        		{id:'docs',pos:{x:2,y:1}},
-        		{id:'sheets',pos:{x:3,y:1}},
-        		{id:'draw',pos:{x:4,y:1}},
-        		{id:'youtube',pos:{x:5,y:1}},
-        		{id:'contacts',pos:{x:6,y:1}},
-        		{id:'chrome',pos:{x:7,y:1}},
+        		{id:'compute',pos:{x:2,y:1}},
+        		{id:'play',pos:{x:3,y:1}},
+        		{id:'docs',pos:{x:4,y:1}},
+        		{id:'sheets',pos:{x:5,y:1}},
+        		{id:'draw',pos:{x:6,y:1}},
+        		{id:'youtube',pos:{x:7,y:1}},
+        		{id:'contacts',pos:{x:8,y:1}},
+        		{id:'chrome',pos:{x:9,y:1}},
         	],
         	[
-        		{id:'gmail',pos:{x:0,y:2}},
-        		{id:'playstore',pos:{x:1,y:2}},
-        		{id:'movies',pos:{x:2,y:2}},
-        		{id:'hangout',pos:{x:3,y:2}},
-        		{id:'drive',pos:{x:4,y:2}},
-        		{id:'news',pos:{x:5,y:2}},
-        		{id:'wallet',pos:{x:6,y:2}},
-        		{id:'devs',pos:{x:7,y:2}},
+        		{id:'gmail',pos:{x:2,y:2}},
+        		{id:'playstore',pos:{x:3,y:2}},
+        		{id:'movies',pos:{x:4,y:2}},
+        		{id:'hangout',pos:{x:5,y:2}},
+        		{id:'drive',pos:{x:6,y:2}},
+        		{id:'news',pos:{x:7,y:2}},
+        		{id:'wallet',pos:{x:8,y:2}},
+        		{id:'devs',pos:{x:9,y:2}},
         	]
         ];
 
@@ -118,7 +130,71 @@ var DevFestCountDown = DevFestCountDown || function(){
         }
     }
 
+    function processMoveLogos(){
+    	if (directionLogos < 0 ){
+    		if (mapLogos[0][0].pos.x > 0){
+    			mapLogos.forEach(function(cols){
+    				cols.forEach(function(cel){
+    					cel.pos.x--;
+    				});
+    			});
+    		}else{
+    			directionLogos = 1;
+    			let rowNum = mapLogos[0][0].pos.y;
+    			mapLogos.forEach(function(cols){
+    				cols.forEach(function(cel){
+    					cel.pos.y+= rowNum === 0 ? 1 : -1;
+    				});
+    			});
+    		}
+    	}else{
+    		if (mapLogos[0][0].pos.x < 4){
+    			mapLogos.forEach(function(cols){
+    				cols.forEach(function(cel){
+    					cel.pos.x++;
+    				});
+    			});
+    		}else{
+    			directionLogos = -1;
+    			let rowNum = mapLogos[0][0].pos.y;
+    			mapLogos.forEach(function(cols){
+    				cols.forEach(function(cel){
+    					cel.pos.y+= rowNum === 0 ? 1 : -1;
+    				});
+    			});
+    		}
+    	}
+    	setTimeout(processMoveLogos, TIME_ANIMATION);
+    }
+
+    function processMoveSpaceShip(){
+    	if (directionSpaceShip < 0){
+    		if (positionSpaceShip.x > 0){
+    			positionSpaceShip.x--;
+    		}else{
+    			positionSpaceShip.x++;
+    			directionSpaceShip = 1;
+    		}
+    	}else{
+    		if (positionSpaceShip.x < 11){
+    			positionSpaceShip.x++;
+    		}else{
+    			positionSpaceShip.x--;
+    			directionSpaceShip = -1;
+    		}
+    	}
+    	setTimeout(processMoveSpaceShip, TIME_ANIMATION_SPACESHIP);
+    }
+
+    function processMoveShoot(){
+    	positionShoot.y--;
+    	if (processMoveShoot > 0){
+    		setTimeout(processMoveShoot, TIME_SHOOT);
+    	}
+    }
+
     function runAnimation(){
+    	context.clearRect(0, 0, canvas.width, canvas.height);
     	mapLogos.forEach(function(cols){
     		cols.forEach(function(cel){
     			context.drawImage(images['logos']
@@ -141,10 +217,27 @@ var DevFestCountDown = DevFestCountDown || function(){
 			, imgSpaceShip.width // swidth clipping de l'image originale
 			, imgSpaceShip.height // sheight clipping de l'image originale
 			, positionSpaceShip.x * (SIZE_LOGO_DEST + MARGIN) // x Coordonnées dans le dessin du Model.ui.canvas
-			, positionSpaceShip.y * (SIZE_LOGO_DEST + MARGIN)// y Coordonnées dans le dessin du Model.ui.canvas
+			, NB_ROW  * (SIZE_LOGO_DEST + MARGIN)// y Coordonnées dans le dessin du Model.ui.canvas
 			, SIZE_LOGO_DEST // width taille du dessin
 			, SIZE_LOGO_DEST * (imgSpaceShip.height / imgSpaceShip.width) // height taille du dessin
 			);
+
+    	if (positionShoot.x >= 0){
+    		context.fillStyle = "white";
+    		context.fillRect(
+    			positionShoot.x * (SIZE_LOGO_DEST + MARGIN) + 20, // X d'origine
+    			(positionShoot.y - 1) * (SIZE_LOGO_DEST + MARGIN), //Y d'origine
+    			5, // width
+    			20 // height
+    		);
+
+    		context.fillRect(
+    			positionShoot.x * (SIZE_LOGO_DEST + MARGIN) + 20, // X d'origine
+    			(positionShoot.y - 1) * (SIZE_LOGO_DEST + MARGIN) - 25, //Y d'origine
+    			5, // width
+    			20 // height
+    		);
+    	}
 
     	window.requestAnimationFrame(runAnimation);
     }
@@ -153,6 +246,13 @@ var DevFestCountDown = DevFestCountDown || function(){
 	function pageLoad(){
 		canvas = document.getElementById('animation');	
 		context = canvas.getContext('2d');
+		let headerRect = document.querySelector('header').getBoundingClientRect();
+		let footerRect = document.querySelector('footer').getBoundingClientRect();
+
+		canvas.height = footerRect.top - headerRect.bottom;
+		canvas.width = (SIZE_LOGO_DEST + MARGIN) * 12;
+
+		NB_ROW = Math.floor(canvas.height / (SIZE_LOGO_DEST + MARGIN));
 
 		audioElt = document.getElementById('playlist');
 		loadSprites([
@@ -161,6 +261,14 @@ var DevFestCountDown = DevFestCountDown || function(){
 		]).then(function(){
 			nextSong();
 			runAnimation();
+			processMoveLogos();
+			processMoveSpaceShip();
+
+			setTimeout(function(){
+				positionShoot.x =  positionSpaceShip.x;
+				positionShoot.y = positionSpaceShip.y;
+				processMoveShoot();
+			},2000)
 		});
 	}
 
